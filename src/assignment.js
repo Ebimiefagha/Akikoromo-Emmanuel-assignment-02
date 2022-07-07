543bbe3f90b34e0f1e53dd1548feb4f2e94ef705
Ebimiefagha <107049098+Ebimiefagha@users.noreply.github.com>
2022-07-07 07:49:42 +0000

    attempt-3

--- src/assignment.js
+++ src/assignment.js
@@ -9,14 +9,19 @@ const assignment = {};
  * 
  * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
  */
+// @param {Array} arrayOfNumbers the array of numbers to sum
+// @returns number the sum of the numbers
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let numbers = 0;
+for (let i  of arrayOfNumbers)         {
+        numbers += i;
+    }
+return numbers;
+  }
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  arrayOfNumbers = arrayOfNumbers.filter ( even => (even % 2 === 0))
+return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
+// console.log(countEvenNumbers)
 
 /**
  * Challenge - 3
@@ -46,9 +53,9 @@ function countEvenNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
-}
+// function celsiusToFahrenheit(arrayOfNumbers) {
+//     return arrayOfNumbers;
+// }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
